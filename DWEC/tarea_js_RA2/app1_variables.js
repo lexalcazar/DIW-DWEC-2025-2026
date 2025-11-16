af6b125b075cff4a6fad9117ecf0507546519b7a
Alex Alcázar <aalcazarprogramacion@gmail.com>
2025-11-17 00:16:06 +0100

    Primer commit tarea 2

---
+++ DWEC/tarea_js_RA2/app1_variables.js
@@ -0,0 +1,25 @@
+//---------------------------------
+//Declaracion de variables
+//---------------------------------
+// Lasavariable tipo let las utilizamos para que su valor prevalezca en todo el script 
+// y no sólo en determinados bloques.
+//Declaramos una variable numerica edad en la que guardaremos números.
+let edad=40;
+//Declaramos una variable tipo String en la que se guarda texto.
+let nombre="Alex";
+// Declaramos una variable booleana con el valor true.
+let esMayorDeEdad=true;
+// Declaramos una variable const tipo float. 
+// este tipo de variable se declara cuando el valor que almacenamos no va a cambiar
+// ni vamos a realizar ningun tipo de operacion que lo vaya a modificar.
+const iva=0.21;
+
+//------------------------------------------------------------------
+//Operadores
+//-----------------------------------------------------------------
+
+let numero1=10;
+let numero2=5;
+let resultado=0;
+resultado=numero1+numero2;
+console.log(resultado);
