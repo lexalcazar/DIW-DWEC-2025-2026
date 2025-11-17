6c4854c36570ed66add9cea970e965b478712ff8
Alex Alcázar <aalcazarprogramacion@gmail.com>
2025-11-17 01:30:38 +0100

    Commit con segundo script

--- DWEC/tarea_js_RA2/app1_variables.js
+++ DWEC/tarea_js_RA2/app1_variables.js
@@ -21,5 +21,46 @@ const iva=0.21;
 let numero1=10;
 let numero2=5;
 let resultado=0;
-resultado=numero1+numero2;
-console.log(resultado);
+resultado=numero1+numero2;// suma
+console.log("Resultado de la suma: "+ numero1+" + "+numero2+" = "+resultado);
+
+resultado=numero1-numero2;//resta
+console.log("Resultado de la resta: "+ numero1+" - "+numero2+" = "+resultado);
+
+resultado=numero1*numero2;//multiplicación
+console.log("Resultado de la multiplicación: "+ numero1+" * "+numero2+" = "+resultado);
+
+resultado=numero1/numero2;//división
+console.log("Resultado de la división: "+ numero1+" / "+numero2+" = "+resultado);
+
+// variables iva
+
+let precioSinIva=100;
+let precioConIva=0;
+precioConIva=precioSinIva+(precioSinIva*iva); // multiplicamos el iva por el precio base y lo sumamos al precio sin IVA
+console.log("Hola "+"Alexander "+"el precio final es: "+precioConIva);// concatenación
+
+//---------------------------------------------------------------------
+//Ámbito de variables
+//---------------------------------------------------------------------
+
+let contador=0;
+if(true){
+    let contador=99;
+    console.log("Contador dentro del if: "+contador);// Contador declarado dentro del if
+}
+console.log("Contador fuera del if: "+contador); // contador de fuera
+
+//---------------------------------------------------
+//Convarsión de tipos
+//---------------------------------------------------
+
+let numeroComoTexto="50";// declaramos la variable
+numeroComoTexto=numeroComoTexto+1;// ambos se tratan como un texto y se concatenan
+console.log("Sumando uno a 50 en tipo texto: "+numeroComoTexto);
+console.log("Tipo de dato: "+typeof numeroComoTexto);// tipo de dato almacenado
+
+numeroComoTexto=Number(numeroComoTexto);//conversión a número
+console.log("Tipo de dato tras la conversión: "+typeof numeroComoTexto);
+numeroComoTexto=numeroComoTexto+6;// aqui se suma uno 
+console.log("Sumando seis a 501 en tipo número: "+numeroComoTexto);
---
+++ DWEC/tarea_js_RA2/app2_decisiones.js
@@ -0,0 +1,41 @@
+//Programa de clasificación de notas
+
+//Declara una variable let nota = 7; (puedes poner el número que quieras).
+
+//Usando if / else if / else, muestra con console.log():
+
+//"Insuficiente" si la nota es menor que 5
+
+//"Suficiente" si está entre 5 y 5.9
+
+//"Bien" si está entre 6 y 6.9
+
+//"Notable" si está entre 7 y 8.9
+
+//"Sobresaliente" si es 9 o más
+
+//Añade comentarios explicando cada condición.
+
+let nota=10;
+
+if(nota<5){ // si nota es menor a 5
+
+    console.log("Has obtenido un: "+ nota+" Insuficiente"); //imprime Insuficiente
+
+    }else if(nota<6 && nota>=5){ // si es menor que 6 y mayor que cinco
+
+        console.log("Has obtenido un: "+ nota+" Suficinete");// imprime suficinete
+
+        }else if(nota<7 && nota>=6){ // si es menor que 7 y mayor o igual a 6
+
+            console.log("Has obtenido un: "+ nota+" Bien"); // imprime bien
+
+            }else if(nota>=7 && nota<=8.9){ // si es mayor o igual a 7 y menor que 8,9
+
+                console.log("Has obtenido un: "+ nota+" Notable"); // imprime notable
+
+                }else if(nota>=9){ // si nota es mayor o igual a 9
+
+                    console.log("Has obtenido un: "+ nota+" Sobresaliente"); // imprime sobresaliente
+
+                    }
