34506170e58079f8498be1ce3ccd524b21c74a33
Alex Alcázar <aalcazarprogramacion@gmail.com>
2025-11-29 23:00:19 +0100

    ejercicio 7

---
+++ DWEC/TEMA_3/laboratorio_objetos_js/ej7.js
@@ -0,0 +1,16 @@
+/**
+ * Ejercicio 7
+ * @author Alexander Alcázar Cecilia
+ */
+
+let mensaje="Hola que tal";
+let noDeclarada=true;
+console.log(mensaje);
+console.log("A continuacion voay a imprimir variable no declarada");
+console.log(noDeclarada);
+let contador=mensaje.longitud;// error creado
+console.log(contador.length);//error creado
+/*do{
+    console.log(contador+" es <= "+mensaje.length);
+    contador++;
+}while(contador<=mensaje.length)*/
